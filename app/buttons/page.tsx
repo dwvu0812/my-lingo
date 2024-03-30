7ef5d3c0d4cbd2798937eeecb1016e149b7ae1bb
Khanh Du Vu <khanhdu1211@gmail.com>
2024-03-30 20:01:11 +0700

    initial

---
+++ app/buttons/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+const ButtonsPage = () => {
+  return (
+    <div
+      className="p-4 space-y-4 flex flex-col
+        max-w-[200px]"
+    >
+      <Button variant={"primary"}>Primary</Button>
+      <Button variant={"primaryOutline"}>Primary Outline</Button>
+    </div>
+  );
+};
+
+export default ButtonsPage;
